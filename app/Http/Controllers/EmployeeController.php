5db458cfcd69a2c725faf1825909f57d5cecdf66
tengkusyafiq <tengku.mohd.syafiq@gmail.com>
2020-02-17 19:28:38 +0800

    api to see all employees, paginated

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Employee;
 use App\Http\Resources\EmployeeResource;
+use App\Http\Resources\EmployeeResourceCollection;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Undocumented function.
-     */
     public function show(Employee $employee): EmployeeResource
     {
         return new EmployeeResource($employee);
+    }
+
+    public function index(): EmployeeResourceCollection
+    {
+        return new EmployeeResourceCollection(Employee::paginate());
     }
 }

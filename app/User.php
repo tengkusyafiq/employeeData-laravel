5084031eddfa7e2596b773afae99f6243dda2353
tengkusyafiq <tengku.mohd.syafiq@gmail.com>
2020-02-17 22:35:44 +0800

    setup passport

--- app/User.php
+++ app/User.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.

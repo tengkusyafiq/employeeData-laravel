7202d18ed389d4bdf8c8ac784def2e522b3a8283
tengkusyafiq <tengku.mohd.syafiq@gmail.com>
2020-02-17 17:44:07 +0800

    test fix api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/employee', function () {
+    return [
+        'first_name' => 'John',
+        'last_name' => 'Wick',
+    ];
 });

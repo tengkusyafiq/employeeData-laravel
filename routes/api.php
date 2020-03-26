b179613b05baec84cea965937de659ee307d4ef8
mohdsyafiq.hadi <mohdsyafiq.hadi@phillips.com>
2020-03-26 16:52:47 +0800

    improve routes

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
 
-Route::prefix('v1')->name('v1.')->group(function () {
+Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1', 'as' => 'v1.'], function () {
     // get employee data, use apiResource to make it restful
-    Route::apiResource('employee', 'Api\v1\EmployeeController')->middleware('auth:api');
-    Route::post('employee/import', 'Api\v1\EmployeeController@import')->name('employee.import')->middleware('auth:api');
+    Route::apiResource('employee', 'EmployeeController')->middleware('auth:api');
+    Route::post('employee/import', 'EmployeeController@import')->name('employee.import')->middleware('auth:api');
 });
